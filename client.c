e3c6b2623cd6874dd6b6c12ba760d1d7ca5093f3
nola-a <antonino.nolano@gmail.com>
2022-01-22 12:53:06 +0100

    removed unused import

--- client.c
+++ client.c
@@ -28,12 +28,6 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <poll.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
 
 #include "client.h"
 
